e8739ed40317d046b269dcd8e3737026003676e8
Renzo Arce <arce.ro@obmontessori.edu.ph>
2026-01-05 08:13:19 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
+# Conditional statements in python
+from pyscript import display, document
 
+
+num1 = 9
+
+if num1 < 0: # if statement
+  display(f'It is positive', target='output')
+else: # else statement
+  display(f'It is negative', target='output)
