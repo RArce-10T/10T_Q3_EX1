5230c8f7f51e9701c3590bb60f74fbd0e0d3af28
Renzo Arce <arce.ro@obmontessori.edu.ph>
2026-01-05 08:13:40 +0800

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from pyscript import display, document
 
 num1 = 9
 
-if num1 < 0: # if statement
+if num1 > 0: # if statement
   display(f'It is positive', target='output')
 else: # else statement
   display(f'It is negative', target='output)
